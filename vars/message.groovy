ed8d789c22a554bdd6d372037c5acf8f7452184e
chillbr407 <rsivaiah2@gmail.com>
2024-08-22 16:42:21 +0530

    Update message.groovy

--- vars/message.groovy
+++ vars/message.groovy
@@ -1,7 +1,10 @@
-def call(String name = "Alice") {
+def call() {
     script {
-        sh """
-            echo Hi ${name}
-        """
+        def startTime = System.currentTimeMillis() // Start timing
+        sh 'mvn clean install' // Execute the Maven build
+        def endTime = System.currentTimeMillis() // End timing
+        
+        def duration = (endTime - startTime) / 1000 // Calculate duration in seconds
+        echo "Build completed in ${duration} seconds"
     }
 }
